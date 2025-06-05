c4ca6842cd314370d7e4b7255bda1224f17eb8bf
@Anderson-limaPereira <161944582+Anderson-limaPereira@users.noreply.github.com>
2025-06-05 19:37:52 -0300

    dev burguer

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: import.meta.env.VITE_API_URL, /*import.meta.env.VITE_API_URL,*/ 
 });
 
 
