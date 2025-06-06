ba049fa27be61908086ef180c0f9004b4ba5c9bb
@Anderson-limaPereira <161944582+Anderson-limaPereira@users.noreply.github.com>
2025-06-05 22:18:04 -0300

    alterado base url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL, /*import.meta.env.VITE_API_URL,*/ 
+    baseURL: 'https://devburger-api-fvyv.onrender.com', /*import.meta.env.VITE_API_URL,*/ 
 });
 
 
